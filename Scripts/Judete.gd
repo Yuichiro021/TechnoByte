ce12343a0e4941b4c32645782c4fe8860e77a9ec
CMarius0 <mariuscacior@gmail.com>
2024-04-13 19:11:34 +0300

    update color

--- Scripts/Judete.gd
+++ Scripts/Judete.gd
@@ -10,7 +10,7 @@ extends Node2D
 func _ready():
 	for child in get_children():
 		child.set_meta("Pollution",0)
-		child.self_modulate = Color(0.3, 0 ,0, 0.0)
+		child.self_modulate = Color(118.0/255.0, 137.0/255.0, 72.0/255.0, 0.0)
 	#for child in get_children():
 	#	print(child.name," ",child.get_meta("Pollution"))
 	timer.connect("timeout",increase_pollution)
@@ -19,7 +19,7 @@ func increase_pollution():
 	var sprite = get_child(rng.randi_range(0, get_children().size()-1))
 	sprite.set_meta("Pollution",sprite.get_meta("Pollution") +1)
 	print(sprite.name, " ", sprite.get_meta("Pollution"))
-	sprite.self_modulate = Color(0.3, 0, 0, float(sprite.get_meta("Pollution"))/10.0)
+	sprite.self_modulate = Color(118.0/255.0, 137.0/255.0, 72.0/255.0, float(sprite.get_meta("Pollution"))/10.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
