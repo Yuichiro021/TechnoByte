f8be106c025e136892c6907e14d4d995188de59b
CMarius0 <mariuscacior@gmail.com>
2024-04-13 17:48:44 +0300

    added incrementing pollution

--- Scripts/Judete.gd
+++ Scripts/Judete.gd
@@ -1,22 +1,32 @@
 class_name Judete
 extends Node2D
 
+@onready var rng = RandomNumberGenerator.new()
+
+@onready var timer = $"../../Timer" as Timer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
-		child.set_meta("Pollution",1)
-	for child in get_children():
-		print(child.name," ",child.get_meta("Pollution"))
+		child.set_meta("Pollution",0)
+	#for child in get_children():
+	#	print(child.name," ",child.get_meta("Pollution"))
+	timer.connect("timeout",increase_pollution)
 
+func increase_pollution():
+	var sprite = get_child(rng.randi_range(0, get_children().size()-1))
+	sprite.set_meta("Pollution",sprite.get_meta("Pollution") +1)
+	print(sprite.name, " ", sprite.get_meta("Pollution"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	for child in get_children():
-		pass
+	pass
+	#print( rng.randi_range(0,get_children().size() - 1) );
+
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1:
 			for child in get_children():
-				pass
+				print(child.position.x, " ", child.position.y)
