ce9f487a7bfe21ae61a25bee65daf3bf23c359ba
CMarius0 <mariuscacior@gmail.com>
2024-04-13 19:28:50 +0300

    change poz

--- Scripts/Judete.gd
+++ Scripts/Judete.gd
@@ -22,6 +22,10 @@ func increase_pollution():
 	print(sprite.name, " ", sprite.get_meta("Pollution"))
 	sprite.self_modulate = Color(118.0/255.0, 137.0/255.0, 72.0/255.0, float(sprite.get_meta("Pollution"))/10.0)
 
+func lower_pollution(sprite):
+	sprite.set_meta("Pollution",0)
+	sprite.self_modulate = Color(118.0/255.0, 137.0/255.0, 72.0/255.0, float(sprite.get_meta("Pollution"))/10.0)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -41,4 +45,5 @@ func _input(event):
 					if Geometry2D.is_point_in_polygon(event.position,global_points):
 						if not click_registered:
 							print("Clicked on: ",child.get_name())
+							lower_pollution(child)
 							click_registered = true
